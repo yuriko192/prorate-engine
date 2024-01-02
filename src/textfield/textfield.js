fd12d6f20960776a9364a41a0192176dddc14a66
Hafidzil Khairi <hafidzilkhairi00@gmail.com>
2024-01-02 15:51:04 +0700

    inital commit

---
+++ src/textfield/textfield.js
@@ -0,0 +1,11 @@
+
+import { TextField as TextFieldMUI } from '@mui/material'
+import React from 'react';
+
+const TextField = () => {
+    return (
+        <TextFieldMUI id="outlined-basic" label="label" variant="outlined" />
+    )
+}
+
+export default TextField;
