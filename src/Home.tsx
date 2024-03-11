48b5b36a4b40a734383762a317056139b137e8d0
yuriko192 <ivanwijaya19@yahoo.co.id>
2024-03-11 21:26:44 +0700

    refactor some components to scss

--- src/Home.tsx
+++ src/Home.tsx
@@ -26,6 +26,7 @@ import {ColorModeContext} from "./context";
 import {currFormatter, GetLocalStorage, RemoveLocalStorage, SetLocalStorageWithExpiry} from "./utils";
 import {DefaultLocalStorageExpiry, LocalItemListKey} from "./const";
 import {ShareDialog} from "./components/ShareDialog";
+import './Home.scss';
 
 
 function Home() {
@@ -135,36 +136,36 @@ function Home() {
 
     return (
         <React.Fragment>
-            <Grid container height={'100vh'} direction={'column'}>
-                <Grid item>
-                    <AppBar
-                        color='default'
-                        elevation={0}
-                        sx={{
-                            position: 'relative',
-                            borderBottom: (t) => `1px solid ${t.palette.divider}`
-                        }}
+            <div className="MainContainer">
+                <AppBar
+                    color='default'
+                    elevation={0}
+                    sx={{
+                        position: 'relative',
+                        borderBottom: (t) => `1px solid ${t.palette.divider}`
+                    }}
+                >
+                    <Toolbar
                     >
-                        <Toolbar
-                        >
-                            <Typography variant="h6" color="inherit" flexGrow={1} noWrap>
-                                Prorate Engine
-                            </Typography>
-                            <Button sx={{ml: 1}} onClick={handleClear} variant={'contained'} color="error">
-                                <DeleteIcon/> Clear
-                            </Button>
-                            <Button sx={{ml: 1}} onClick={colorMode.toggleColorMode} color="inherit">
-                                {theme.palette.mode} mode {theme.palette.mode === 'dark' ? <Brightness7Icon/> :
-                                <Brightness4Icon/>}
-                            </Button>
-                        </Toolbar>
-                    </AppBar>
-                </Grid>
-                <Grid item flex={1} sx={{py: 2,}}>
+                        <Typography variant="h6" color="inherit" flexGrow={1} noWrap>
+                            Prorate Engine
+                        </Typography>
+                        <Button sx={{ml: 1}} onClick={handleClear} variant={'contained'} color="error">
+                            <DeleteIcon/> Clear
+                        </Button>
+                        <Button sx={{ml: 1}} onClick={colorMode.toggleColorMode} color="inherit">
+                            {theme.palette.mode} mode {theme.palette.mode === 'dark' ? <Brightness7Icon/> :
+                            <Brightness4Icon/>}
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+                <div className="ContentContainer">
                     <Container maxWidth="md" sx={{height: '100%', p: {xs: 0}}}>
                         <Paper elevation={3} sx={{p: {xs: 2, md: 3}, height: '100%'}} square={false}>
-                            <Grid container direction={'column'} columnSpacing={1} rowSpacing={1} sx={{height: '100%'}}>
-                                <Grid item container justifyContent={'space-between'} columnSpacing={1} rowSpacing={1}>
+                            <Grid container direction={'column'} columnSpacing={1} rowSpacing={1}
+                                  sx={{height: '100%'}}>
+                                <Grid item container justifyContent={'space-between'} columnSpacing={1}
+                                      rowSpacing={1}>
                                     <Grid item xs={12} md={6}>
                                         <TextField
                                             value={(!discountAmount || discountAmount <= 0) ? '' : discountAmount}
@@ -180,7 +181,8 @@ function Home() {
                                         />
                                     </Grid>
                                     <Grid item xs={12} md={6}>
-                                        <TextField value={(!otherFee || otherFee <= 0) ? '' : otherFee} variant='outlined'
+                                        <TextField value={(!otherFee || otherFee <= 0) ? '' : otherFee}
+                                                   variant='outlined'
                                                    type='number' label='Other fee' fullWidth
                                                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
                                                        if (!event.target.valueAsNumber) {
@@ -213,7 +215,8 @@ function Home() {
                                                                 <TableCell>{product.ProductName}</TableCell>
                                                                 <TableCell
                                                                     align='center'>{currFormatter.format(product.ProductPrice)}</TableCell>
-                                                                <TableCell align='center'>{product.Quantity}</TableCell>
+                                                                <TableCell
+                                                                    align='center'>{product.Quantity}</TableCell>
                                                                 <TableCell
                                                                     align='center'>{currFormatter.format(product.FinalPrice)}
                                                                     <a style={{
@@ -225,12 +228,14 @@ function Home() {
                                                                     <Grid container columnSpacing={1} rowSpacing={1}
                                                                           justifyContent={'center'}>
                                                                         <Grid item>
-                                                                            <Button variant="contained" onClick={e => {
-                                                                                handleClickEdit(idx)
-                                                                            }}><EditIcon/></Button>
+                                                                            <Button variant="contained"
+                                                                                    onClick={e => {
+                                                                                        handleClickEdit(idx)
+                                                                                    }}><EditIcon/></Button>
                                                                         </Grid>
                                                                         <Grid item>
-                                                                            <Button variant="contained" color='error'
+                                                                            <Button variant="contained"
+                                                                                    color='error'
                                                                                     onClick={e => {
                                                                                         handleDelete(idx)
                                                                                     }}><DeleteIcon/></Button>
@@ -269,8 +274,38 @@ function Home() {
                             </Grid>
                         </Paper>
                     </Container>
-                </Grid>
-            </Grid>
+                </div>
+                <div className="Fixed Background">
+                    <div className="custom-shape-divider-top-1710167091">
+                        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"
+                             preserveAspectRatio="none">
+                            <path
+                                d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+                                opacity=".25" className="shape-fill"></path>
+                            <path
+                                d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
+                                opacity=".5" className="shape-fill"></path>
+                            <path
+                                d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
+                                className="shape-fill"></path>
+                        </svg>
+                    </div>
+                    <div className="custom-shape-divider-bottom-1710166162">
+                        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"
+                             preserveAspectRatio="none">
+                            <path
+                                d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+                                opacity=".25" className="shape-fill"></path>
+                            <path
+                                d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
+                                opacity=".5" className="shape-fill"></path>
+                            <path
+                                d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
+                                className="shape-fill"></path>
+                        </svg>
+                    </div>
+                </div>
+            </div>
             <AddItemDialog
                 open={open}
                 onClose={handleClose}
